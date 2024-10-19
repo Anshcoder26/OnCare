8882005624a61c6391050dece84d327a049c86de
Ansh Agarwal <anshscholarsden@gmail.com>
2024-10-20 01:25:28 +0530

    Register Page

---
+++ Oncare/oncare/src/Components/Login/Signup/Login.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import "./Login.css";
+import { FaMale } from "react-icons/fa";
+import { FaFemale } from "react-icons/fa";
+
+const Login = () => {
+
+    return(
+        <div className='foram'>
+         <form action=''>
+         <h1 className='register'>REGISTER</h1>
+         <div className='input-box'>
+             <input type='text' name="name" placeholder='Name' required/>
+         </div>
+            <br/>
+            <div className='input-box'>
+                <input type='number' name='Contact Number' placeholder='Contact Number' required/>
+            </div>
+            <br/>
+            <div className='input-box'>
+                 <input type='text' name='email' placeholder='Email ID' required/>
+            
+            </div>
+            <br/>
+            <div className='input-box'>
+                 <input type="number" name='Age' placeholder='Age' required/>
+            </div>
+            <br/>
+            <label className='nana'>Gender</label><br/>
+
+            <button className='gender' type='male'><FaMale className='icon' /></button><button type='female' className='gender1' ><FaFemale className='icon'/></button>
+            <br/>
+            <button type='submit' className='enroll'>Enroll</button>
+            </form>
+        </div>
+    )
+}
+
+
+export default Login;
