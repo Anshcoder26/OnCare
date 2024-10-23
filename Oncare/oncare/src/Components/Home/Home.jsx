0772cca81951138a23f07612cb5ba4497103547a
Ansh Agarwal <anshscholarsden@gmail.com>
2024-10-23 21:43:44 +0530

    minor changes

--- Oncare/oncare/src/Components/Home/Home.jsx
+++ Oncare/oncare/src/Components/Home/Home.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./Home.css";
 
 const Home = () => {
 
     return(
-        <div>
-            <nav>
-
-            </nav>
-        </div>
+        <nav>
+            <div>
+                <Link to={"/"}>OnCare  </Link>
+                <Link to={"/Register"}>Register  </Link>
+                <Link to={"/Login"}>Login  </Link>
+            </div>
+        </nav>
     )
 }
 
